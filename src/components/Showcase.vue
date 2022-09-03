24725fcfcfe5c611a30f66a43e4e5de79fd6e532
akkayy <techakayy@gmail.com>
2022-09-03 21:33:14 +1000

    Added ShowCase.vue component

---
+++ src/components/Showcase.vue
@@ -0,0 +1,86 @@
+<script setup>
+	import logos from '../images/framework-logos.png'
+	import logosSmall from '../images/framework-logos-small.png'
+	import { ref } from 'vue'
+
+	const props = defineProps({
+		heading: {
+			type: String,
+			// required: true,
+			default: 'Welcome To The Astro Crash Course!'
+		},
+		text: {
+			type: String,
+			// required: true,
+			default: `Build super fast HTML-first websites using your favorite JavaScript frameworks`
+		}
+	})
+</script>
+
+<template>
+	<section class="showcase">
+		<div class="showcase-text">
+			<h1>{{ heading }}</h1>
+			<p>{{ text }}</p>
+		</div>
+		<div class="showcase-img">
+			<img class="logos" :src="logos" alt="" />
+			<img class="logos-small" :src="logosSmall" alt="" />
+		</div>
+
+		<p class="showcase-demo">
+			<small>This is a demo website used for the YouTube crash course</small>
+		</p>
+	</section>
+</template>
+
+<style scoped>
+	/* Showcase */
+	.showcase {
+		height: auto;
+		background-color: var(--color-secondary);
+		background-image: linear-gradient(
+			to bottom,
+			var(--color-primary),
+			rgb(15 23 42 / 0)
+		);
+		padding: 30px;
+	}
+
+	.showcase-blog {
+		min-height: 300px;
+	}
+
+	.showcase-text {
+		max-width: 500px;
+		color: #fff;
+		text-align: center;
+		margin: 50px auto;
+	}
+
+	.showcase h1 {
+		font-size: 50px;
+		color: #fff;
+		margin-bottom: 20px;
+	}
+
+	.showcase p {
+		font-size: 24px;
+		margin-bottom: 20px;
+	}
+
+	.showcase-img {
+		text-align: center;
+	}
+
+	.showcase-img img {
+		border-radius: 10px;
+		margin: auto;
+	}
+
+	.showcase-demo {
+		color: #fff;
+		text-align: center;
+		font-weight: 300;
+	}
+</style>
